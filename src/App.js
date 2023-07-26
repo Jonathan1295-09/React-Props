34519756669a146e61448ca3ee4d7a988930ce91
Jonathan1295-09 <jcasasola955@gmail.com>
2023-07-26 00:10:34 -0500

    initial commit

---
+++ src/App.js
@@ -0,0 +1,56 @@
+import logo from './logo.svg';
+import './App.css';
+
+
+  
+function App() {
+
+  const movies = [
+    {
+      name: "Transformers",
+      year: "2007",
+      rating: "PG-13",
+      image: "https://images.app.goo.gl/EJrZeDGXiKRSfJFFA"
+
+    },
+    {
+      name: "John Wick Chapter 4",
+      year: "2023",
+      rating: "R",
+      image: "https://images.app.goo.gl/aotKfwvxodDxyAgBA"
+
+    },
+    {
+      name: "Rush",
+      year: "2013",
+      rating: "R",
+      image: "https://images.app.goo.gl/pqCWo7KSZjtcJNnA8"
+
+    },
+    {
+      name: "Man on fire",
+      year: "2004",
+      rating: "R",
+      image: "https://images.app.goo.gl/s8CPv49YK8jcbLmZ8"
+
+    },
+    {
+      name: "Sicario",
+      year: "2015",
+      rating: "R",
+      image: "https://images.app.goo.gl/wcpQSzjW7y5JwBTu9"
+
+    },
+  ]
+
+
+  return (
+    <div className="App">
+
+    </div>
+  );
+}
+
+
+
+export default App;
